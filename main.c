d1eef3e2324b59121bcd1b8bdf38a60e8403f8f3
CLARISSASAA19 <clarissa.saa@udla.edu.ec>
2023-01-13 16:40:48 -0500

    PROGRAMA GIT HUB

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 void suma(double a, double b);
 void resta(double a, double b);
 void multiplicacion(double a, double b);
-void division(double a, double b)
+void division(double a, double b);
 int main(){
  double n1, n2;
- printf ("ingrese el primer numero:");
+ printf ("ingrese el primer numero: \n");
  scanf("%lf",&n1);
- printf ("ingrese el segundo numero:");
+ printf ("ingrese el segundo numero: \n");
  scanf("%lf",&n2);
  suma(n1,n2);
  resta(n1,n2);
@@ -19,19 +19,19 @@ return 0;
 }
 void suma(double a, double b){
     double s=a+b;
-    printf("la suma es: %lf",s);
+    printf("la suma es: %.2lf \n",s);
 }
 void resta(double a, double b){
     double s=a-b;
-    printf("la resta es: %lf",s);
+    printf("la resta es: %.2lf \n",s);
 }
 void multiplicacion(double a, double b){
     double s=a*b;
-    printf("la multiplicacion es: %lf",s);
+    printf("la multiplicacion es: %.2lf \n",s);
 }
 void division(double a, double b){
     double s=a/b;
-    printf("la division es: %lf",s);
+    printf("la division es: %.2lf \n",s);
 }
 
 
